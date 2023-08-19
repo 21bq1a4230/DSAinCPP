6666bfe5bade9b8bc9e1925ff87428e5d4d261fd
21bq1a4230 <21bq1a4230@vvit.net>
2023-08-19 18:25:45 +0530

    finding factorial of a number

---
+++ factorial.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int fact = 1;
+
+
+int factorial(int n){
+    if(n<=1){
+        return 1;
+    }
+    int f = 1;
+    for(int i=1;i<=n;i++){
+        f = f*i;
+    }
+    return f;
+}
+
+//this is iterative implementation
+//time complexity - O(n)
+// space and aux space complexity - O(1)
+
+int factorial(int n){
+    if(n<1){
+        return fact;
+    }
+    fact = fact*n;
+    return factorial(n-1);
+}
+
+//this is recursive implementation
+//time complexity - O(n)
+// space and aux complexity - O(1)
+
+int main(){
+    cout << factorial(5);
+    return 0;
+}
